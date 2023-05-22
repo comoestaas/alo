c3f7d9eada4fb3165df630432d04d2edd89d31a9
Daniel <dbenke@hotmail.com>
2023-05-21 21:37:54 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, World!</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <?php
+        echo "Olá, Mundo!";
+        		
+
+?>
+<br>
+<br>
+<a href = "if/inputform.html">clique aqui para acessar inputform.html no diretório if</a>
+<br>
+<a href = "wf/writefile.html">clique aqui para acessar writefile.html no diretório wf</a>
+<br>
+<a href = "wf/wpalavra.html">clique aqui para acessar wpalavra.html no diretório wf</a>
+</body>
+</html>
+
+
